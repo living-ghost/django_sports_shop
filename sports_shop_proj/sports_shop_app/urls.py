bfdf81d93dded259db05db4a41d25901cf78607a
living-ghost <akhiiltkaniiparampiil@gmail.com>
2025-03-24 00:26:23 +0530

    load static setup done for all html pages

--- sports_shop_proj/sports_shop_app/urls.py
+++ sports_shop_proj/sports_shop_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("shop/", views.shop, name="shop"),
+    path("skating/", views.skating, name="skating"),
 ]
--- sports_shop_proj/sports_shop_app/views.py
+++ sports_shop_proj/sports_shop_app/views.py
@@ -4,4 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the sports shop index.")
+    return render(request, "sports_shop_app/index.html")
+
+def about(request):
+    return render(request, "sports_shop_app/about.html")
+
+def contact(request):
+    return render(request, "sports_shop_app/contact.html")
+
+def shop(request):
+    return render(request, "sports_shop_app/shop.html")
+
+def skating(request):
+    return render(request, "sports_shop_app/skating.html")
