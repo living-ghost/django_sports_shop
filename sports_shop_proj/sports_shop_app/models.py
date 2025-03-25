79f1d17819253025d8f2d6c2d2146c68106e1445
living-ghost <akhiiltkaniiparampiil@gmail.com>
2025-03-26 00:18:55 +0530

    register model and db connected

--- sports_shop_proj/sports_shop_app/models.py
+++ sports_shop_proj/sports_shop_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Register(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
